11c5388ffe25bf8e34e9e32160ee51d75c232daa
highb33kay <hiibeekayvibe@gmail.com>
2023-01-31 13:54:17 +0100

    done

---
+++ operators/added.py
@@ -0,0 +1,8 @@
+# first variable
+a = int(input("enter first number: "))
+# collect second variable
+b = int(input("enter second number: "))
+# return the result of sum of both variable
+z = a + b
+# print the result
+print(z)
---
+++ operators/equals.py
@@ -0,0 +1,4 @@
+x = float(input("What is yor input: "))
+y = float(input("what your second input: "))
+z = x > y
+print(z)
