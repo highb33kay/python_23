c74d43fec2025d7ce4424ada12f02241854a6a4b
highb33kay <hiibeekayvibe@gmail.com>
2023-02-13 10:53:21 +0100

    done calculator

--- loops/calculator.py
+++ loops/calculator.py
@@ -1,14 +1,25 @@
 def main():
-    x, y = "", ""
-    while x <= "" and y <= "":
+    x, y, op = "", "", ""
+    while x <= "" and y <= "" and op != "+" and op != "-" and op != "/" and op != "*":
         x = input("Enter a number: ")
         y = input("Enter a second number: ")
+        op = input("Enter an Operator: ")
+
     x, y = int(x), int(y)
-    added(x, y)
+    if op == "+":
+        added(x, y)
+    elif op == "-":
+        subed(x, y)
+    elif op == "/":
+        print("pass in your div function")
 
 
-def added(x, y):
-    print(f"the sum of {x} + {y} = {x+y}")
+def added(x, y,):
+    print(f"the sum of {x} + {y} = {x + y}")
+
+
+def subed(x, y,):
+    print(f"the sum of {x} - {y} = {x - y}")
 
 
 main()
